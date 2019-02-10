e5894a11a86c7ebad6987d4b5fc9c69d58342b4a
김보라 <bora.kim@odinue.net>
2019-02-10 22:49:10 +0900

    Initial commit

---
+++ src/main/java/com/bora/springwebservice/Application.java
@@ -0,0 +1,16 @@
+package com.bora.springwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing //JPA Auditing 활성화
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
